8dde1017a01fedd6ae20f2a68bd8d1adffd4a966
thiago.rosario <thiago.souza@ssp.ba.gov.br>
2026-05-27 19:41:16 -0300

    chore: add core module bindings to service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Core\Application\Interfaces\PythonBridgeEventParserInterface;
+use App\Core\Application\Interfaces\PythonMessageOutputParserInterface;
+use App\Core\Application\Interfaces\PythonMessagePayloadAdapterInterface;
+use App\Core\Application\Interfaces\PythonMessagePayloadMapperInterface;
+use App\Core\Infra\Adapter\PythonMessagePayloadAdapter;
+use App\Core\Infra\Mapper\PythonMessagePayloadMapper;
+use App\Core\Infra\Parser\PythonBridgeEventParser;
+use App\Core\Infra\Parser\PythonMessageOutputParser;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PythonMessagePayloadAdapterInterface::class, PythonMessagePayloadAdapter::class);
+        $this->app->bind(PythonBridgeEventParserInterface::class, PythonBridgeEventParser::class);
+        $this->app->bind(PythonMessagePayloadMapperInterface::class, PythonMessagePayloadMapper::class);
+        $this->app->bind(PythonMessageOutputParserInterface::class, PythonMessageOutputParser::class);
     }
 
     /**
