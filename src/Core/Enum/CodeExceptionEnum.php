c3798d154187a61f2d74fa8e27bc3a76e1c1349b
thiago.rosario <thiago.souza@ssp.ba.gov.br>
2026-06-01 16:29:30 -0300

    feat(core): adiciona exceptions customizadas para validação do Google Sheets e atualiza códigos de erro no CodeExceptionEnum

--- src/Core/Enum/CodeExceptionEnum.php
+++ src/Core/Enum/CodeExceptionEnum.php
@@ -9,4 +9,9 @@ enum CodeExceptionEnum: int
     case MessageNotContent = 1000;
     case OpenAIEmptyResponse = 1001;
     case GoogleSheetRead = 1002;
+
+    case GoogleSpreadsheetIdRequired = 1003;
+    case GoogleSheetIdRequired = 1004;
+    case GoogleSheetGidInvalid = 1005;
+    case GoogleSheetNameRequired = 1006;
 }
--- src/Core/Exception/GoogleSheetReadException.php
+++ src/Core/Exception/GoogleSheetReadException.php
@@ -10,10 +10,15 @@ use Throwable;
 
 class GoogleSheetReadException extends RuntimeException
 {
+    /**
+     * @param  array{gid: int, name: string}|null  $sheet
+     */
     public function __construct(
         string $message = 'Falha ao ler os dados da planilha Google.',
         int $code = CodeExceptionEnum::GoogleSheetRead->value,
-        ?Throwable $previous = null
+        ?Throwable $previous = null,
+        public readonly ?string $spreadsheetId = null,
+        public readonly ?array $sheet = null,
     ) {
         parent::__construct($message, $code, $previous);
     }
