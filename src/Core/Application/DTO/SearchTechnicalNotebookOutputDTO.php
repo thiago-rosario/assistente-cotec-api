52b4d1bae95698d0c7c5de0c8b399d6ad4038040
thiago.rosario <thiago.souza@ssp.ba.gov.br>
2026-06-01 23:01:26 -0300

    feat(core): implement SearchTechnicalNotebook feature with DTOs, use case, and interface definition

---
+++ src/Core/Application/DTO/SearchTechnicalNotebookOutputDTO.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Application\DTO;
+
+readonly class SearchTechnicalNotebookOutputDTO
+{
+    /**
+     * @param  list<array<string, mixed>>  $data
+     */
+    public function __construct(
+        public ?string $term,
+        public int $total,
+        public array $data,
+    ) {}
+}
