d23f5653d94183543bfaa6da167858a90e837847
thiago.rosario <thiago.souza@ssp.ba.gov.br>
2026-06-08 10:28:55 -0300

    feat(core): add DirectWhatsappMessageInterpreterService with rules for SEI process handling and general interpretation

---
+++ src/Core/Application/Interfaces/DirectWhatsappMessageInterpreterServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Application\Interfaces;
+
+use App\Core\Application\DTO\WhatsappMessageInterpretationDTO;
+
+interface DirectWhatsappMessageInterpreterServiceInterface
+{
+    public function interpret(string $message): ?WhatsappMessageInterpretationDTO;
+}
