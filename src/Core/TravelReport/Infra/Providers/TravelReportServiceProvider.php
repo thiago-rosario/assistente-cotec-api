22f99275e13fb05d5336cf407a445b1b8441c1e6
thiago.rosario <thiago.souza@ssp.ba.gov.br>
2026-07-18 22:12:43 -0300

    feat(travel-report): register `TravelReportServiceProvider` in application

---
+++ src/Core/TravelReport/Infra/Providers/TravelReportServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Core\TravelReport\Infra\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TravelReportServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+    }
+}
